ca2044226b5c0acc642473bde316c29e5f637708
VladSelikh <vladikh2405@gmail.com>
2020-10-28 08:55:36 +0300

    feat: complete dream-tem function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let initLetArr = members.map(item => {
+    if (typeof item !== "string") {
+      return '';
+    } else {
+      for (let i = 0; i < item.length; i++) {
+        if (item.charCodeAt(i) > 64 && item.charCodeAt(i) < 91) {
+          return item[i];
+        } else if (item.charCodeAt(i) > 96 && item.charCodeAt(i) < 123) {
+          return String.fromCharCode(item.charCodeAt(i) - 32)
+        }
+      }
+    }
+  });
+  let alphOrdArr = initLetArr.sort();
+  return alphOrdArr.join('');
 };
