1d400b48578ff49146ab5d92696438763f5085d6
Uladzislau <Uladzislau_Selikh@epam.com>
2021-01-23 09:41:05 +0300

    feat: implement what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-    throw new CustomError('Not implemented');
+    if (date === undefined) return "Unable to determine the time of year!"
+    const month = date.getMonth() + 1;
+    if (month && Object.prototype.toString.call(date) === "[object Date]") {
+        return (month === 12 || month < 3)? "winter" : (month >= 3 && month < 6)? "spring" :  (month >= 6 && month < 9)? "summer" : "autumn";
+    } else {
+        throw new Error('Not valid data!');
+    }
 };
