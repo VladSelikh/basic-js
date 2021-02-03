03443bee42053e780efb69ea6ad8ca10e77561c8
Uladzislau <Uladzislau_Selikh@epam.com>
2021-02-03 11:49:06 +0300

    feat: complete simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  chain: [],
+  result: '',
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = ``) {
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain[position - 1] === undefined) {
+      this.chain = [];
+      this.result = '';
+      throw new Error(`Invalid position parameter!`);
+    };
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let tempChain = this.chain;
+    let tempResult = this.result;
+
+    tempChain.forEach((item, index, array) => {
+      tempResult += (index === 0)? `( ${item} )~` : (index === array.length - 1)? `~( ${item} )` : `~( ${item} )~`
+    })
+
+    this.chain = [];
+    this.result = '';
+
+    return tempResult[tempResult.length - 1] === `~`? tempResult.substring(0, tempResult.length - 1) : tempResult;
   }
 };
 
 module.exports = chainMaker;
+
+//console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain() === '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )')
