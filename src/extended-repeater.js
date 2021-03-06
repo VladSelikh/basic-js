3f59b4ace982463ca872a408cc8fa39c78928dfe
Uladzislau Selikh <Uladzislau_Selikh@epam.com>
2021-03-06 13:46:56 +0300

    feat: implement vigenere-cipher function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -7,14 +7,13 @@ module.exports = function repeater(str, options = {
   additionRepeatTimes: 0,
   additionSeparator: `|`
 }) {
-  throw new CustomError('Not implemented');
-    // remove line with error and write your code here
   // let additionalString = options.addition.toString().concat(options.additionSeparator).repeat(options.additionRepeatTimes);
   // let lastAdditionSeparatorIndex = additionalString.lastIndexOf(options.additionSeparator);
   // additionalString = additionalString.substring(0, lastAdditionSeparatorIndex);
   // let resultString = str.toString().concat(additionalString, options.separator).repeat(options.repeatTimes);
   // let lastSeparatorIndex = resultString.lastIndexOf(options.separator);
   // return resultString.substring(0, lastSeparatorIndex);
+  throw new CustomError(`Not implemented!`)
 };
 
 //console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(dir) {
+    this.direction = dir === false? false : true;
   }
+  encrypt(text, key) {
+    if (!text || !key) throw new Error(`The argument list is not correct!`)
+    else {
+    const strToEncrypt = text.toUpperCase();
+    const keyText = key.toUpperCase();
+    let stepInKey = 0;
+    let cipheredText = '';
+    for (let i = 0; i < strToEncrypt.length; i++) {
+      if (strToEncrypt[i].charCodeAt(0) > 64 && strToEncrypt[i].charCodeAt(0) < 91) {
+        cipheredText += String.fromCharCode((strToEncrypt[i].charCodeAt(0) + keyText[stepInKey % keyText.length].charCodeAt(0) - 130) % 26 + 65);
+        stepInKey++;
+      }
+      else cipheredText += strToEncrypt[i];
+    }
+    return this.direction? cipheredText : cipheredText.split('').reverse().join('');
+  }
+  }    
+  decrypt(cipheredText, key) {
+    if (!cipheredText || !key) throw new Error(`The argument list is not correct!`)
+    else {
+    const keyText = key.toUpperCase();
+    let stepInKey = 0;
+    let decryptedText = '';
+    for (let i = 0; i < cipheredText.length; i++) {
+      if (cipheredText[i].charCodeAt(0) > 64 && cipheredText[i].charCodeAt(0) < 91) {
+        decryptedText += String.fromCharCode((cipheredText[i].charCodeAt(0) - keyText[stepInKey % keyText.length].charCodeAt(0) + 26) % 26 + 65);
+        stepInKey++;
+      }
+      else decryptedText += cipheredText[i];
+    }
+    return this.direction? decryptedText : decryptedText.split('').reverse().join('');
+  }
+}
 }
 
 module.exports = VigenereCipheringMachine;
+
+cipher = new VigenereCipheringMachine(false);
+console.log(cipher.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
