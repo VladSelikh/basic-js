482c2ea17826d202e1c853488d57a58ce8a5c1a4
Uladzislau_Selikh <Uladzislau_Selikh@epam.com>
2021-06-05 19:18:36 +0300

    feat: implement extended repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,19 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options = {
-  repeatTimes: 0,
-  separator: `+`,
-  addition: ``,
-  additionRepeatTimes: 0,
-  additionSeparator: `|`
-}) {
-  // let additionalString = options.addition.toString().concat(options.additionSeparator).repeat(options.additionRepeatTimes);
-  // let lastAdditionSeparatorIndex = additionalString.lastIndexOf(options.additionSeparator);
-  // additionalString = additionalString.substring(0, lastAdditionSeparatorIndex);
-  // let resultString = str.toString().concat(additionalString, options.separator).repeat(options.repeatTimes);
-  // let lastSeparatorIndex = resultString.lastIndexOf(options.separator);
-  // return resultString.substring(0, lastSeparatorIndex);
-  throw new CustomError(`Not implemented!`)
-};
+module.exports =  function repeater(str, options) {
+  options.separator = options.separator !== undefined? options.separator : '+';
+  options.additionSeparator = options.additionSeparator !== undefined? options.additionSeparator : '|';
+  options.repeatTimes = options.repeatTimes !== undefined? options.repeatTimes : 1;
+  options.additionRepeatTimes = options.additionRepeatTimes !== undefined? options.additionRepeatTimes : 1;
+  options.addition = options.addition !== undefined? options.addition : '';
 
-//console.log(repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' }));
+  options.addition = options.addition !== null? options.addition : 'null';
+  str = str !== null? str : 'null';
+
+  let additionalString = options.addition.toString().concat(options.additionSeparator).repeat(options.additionRepeatTimes);
+  let lastAdditionSeparatorIndex = additionalString.lastIndexOf(options.additionSeparator);
+  additionalString = additionalString.substring(0, lastAdditionSeparatorIndex);
+  resultString = str.toString().concat(additionalString, options.separator).repeat(options.repeatTimes);
+  let lastSeparatorIndex = resultString.lastIndexOf(options.separator);
+
+  return resultString.substring(0, lastSeparatorIndex);
+};
